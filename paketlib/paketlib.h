f5682804c2d0c21b0181e99f136dc5f7753465a0
Moises Aguirre <aguirremoises@proton.me>
2025-07-14 22:54:39 -0400

    paketlib: Fixed wrong type in Packet_WriteFixed.

--- paketlib/paketlib.h
+++ paketlib/paketlib.h
@@ -16,7 +16,7 @@ extern __mod_reg __str Packet_RvalStr;
 [[call("StkCall"), extern("ACS")]]                      int Packet_GetSize (str pac);
 [[call("StkCall"), extern("ACS")]]                      __str Packet_New (void);
 [[call("StkCall"), extern("ACS"), optional_args(1)]]    __str Packet_Append (__str pac, int val, int width);
-[[call("StkCall"), optional_args(1)]]                   __str Packet_AppendFixed (__str pac, int val, int width);
+[[call("StkCall"), optional_args(1)]]                   __str Packet_AppendFixed (__str pac, __fixed val, int width);
 [[call("StkCall"), extern("ACS")]]                      __str Packet_AppendStr (__str pac, __str val);
 [[call("StkCall"), extern("ACS")]]                      int Packet_Send (__str scrip, __str pac, int client);
 [[call("StkCall"), extern("ACS"), optional_args(1)]]    __str Packet_Read (__str pac, int width);
